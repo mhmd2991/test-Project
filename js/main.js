dfee0d44d9906473335091d712848d74c9d908a7
Mohamad Salimi <mohamad.salimi@hotmail.com>
2021-11-28 14:31:27 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+let btn = document.querySelector(".toggle-menu");
+let icon = document.querySelector(".toggle-menu i");
+let menu = document.querySelector(".menu-bar");
 
+btn.addEventListener("click", function(e) {
+    menu.classList.toggle("active");
+    icon.classList.toggle("fa-times");
+});
